6d1f718c8117ea292c1e90f31a4b52d1608b328e
Fernando <fernando.valdivia.ext@kimiagroup.com>
2023-02-20 20:49:28 +0100

    Merge into master

--- src/app/models/application.model.ts
+++ src/app/models/application.model.ts
@@ -1,8 +1,10 @@
 import {model, Schema} from "mongoose";
-import TripApplication from "../types/application";
+import {ActorModel} from "./actor.model";
+import {TripModel} from "./trip.model";
+import {Application} from "../types/application";
 
-const tripApplicationSchema = new Schema({
-    create_date:{
+const applicationSchema = new Schema({
+    create_date: {
         type: Date,
         default: Date.now
     },
@@ -16,19 +18,43 @@ const tripApplicationSchema = new Schema({
         type: String,
         required: true,
     },
+    explorer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Actor',
+        index: true
+    },
+    trip: {
+        type: Schema.Types.ObjectId,
+        ref: 'Trip',
+        index: true
+    },
     denied: {
         type: Boolean,
         required: false
     },
-    reason:{
+    reason: {
         type: String,
         required: false
     },
-    isPaid:{
+    isPaid: {
         type: Boolean,
         require: false
     }
 })
 
+applicationSchema.pre('save', async function (next) {
+    const actor = await ActorModel.findOne({_id: this.explorer}).exec();
+    if (!(actor?.role.indexOf("EXPLORER") > -1)) {
+        next(new Error('ExplorerRoleError'));
+    }
 
-export default model<TripApplication>('Application', tripApplicationSchema)
+    const trip = await TripModel.findOne({_id: this.trip}).exec();
+    const now = new Date();
+    if (!trip?.published || trip?.start_date < now || trip?.cancelled) {
+        next(new Error('ApplicationError'));
+    }
+    next();
+})
+
+
+export const ApplicationModel = model<Application>('Application', applicationSchema)
--- src/app/routes/application.routes.ts
+++ src/app/routes/application.routes.ts
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import ApplicationController from "../controllers/application.controller";
-import {createValidator} from "../validators/ApplicationValidator";
 import handleValidation from "../middlewares/ValidationMiddleware";
+import {createValidator} from "../validators/ApplicationValidator";
 import {Routes} from "../util/routes.interface";
+import ApplicationController from "../controllers/application.controller";
 
 class ApplicationRoutes implements Routes {
-    public path = "/v1/applications";
+    public path = '/v1/applications';
     public router = Router();
     public controller = new ApplicationController();
 
@@ -14,23 +14,15 @@ class ApplicationRoutes implements Routes {
     }
 
     private initializeRoutes() {
-        this.router.post(
-            this.path,
-            createValidator,
-            handleValidation,
-            this.controller.addTripApplication
-        );
+        this.router.post(this.path, createValidator, handleValidation, this.controller.addApplication);
         this.router.get(this.path, this.controller.getAllApplications);
-        this.router.get(`${this.path}/:id`, this.controller.getApplicationById);
-        this.router.put(
-            `${this.path}/:id`,
-            createValidator,
-            handleValidation,
-            this.controller.updateApplication
-        );
-        this.router.delete(`${this.path}/:id`, this.controller.deleteApplication);
-        this.router.patch(`${this.path}/:id/denied`, this.controller.deniedApplication);
+        this.router.get(`${this.path}:id`, this.controller.getApplicationById);
+        this.router.put(`${this.path}:id`, createValidator, handleValidation, this.controller.updateApplication);
+        this.router.delete(`${this.path}:id`, this.controller.deleteApplication);
+        this.router.patch(`${this.path}:id/denied`, this.controller.deniedApplication)
+        this.router.patch(`${this.path}:id/approved`, this.controller.approvedApplication)
     }
 }
+
 
 export default ApplicationRoutes;
